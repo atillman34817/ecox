2eb6b691a6a73caafa6b7b012d92523f00fa8daa
atillman34817 <adrian.tillman@gmail.com>
2016-05-24 22:47:44 -0400

    Adding Sensor class for testing purposes.

---
+++ src/main/java/edu/spcollege/ecox/datacollection/Sensor.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.spcollege.ecox.datacollection;
+
+/**
+ *
+ * @author tillman.adrian
+ */
+public class Sensor {
+    
+}
