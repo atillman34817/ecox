f8f6d6fa003eb1e52a9e08616d884eccbfd41b04
Student <Student@10.30.80.139>
2016-06-01 18:07:17 -0400

    Added Image and Location class.

--- src/main/java/edu/spcollege/ecox/domain/Image.java
+++ src/main/java/edu/spcollege/ecox/domain/Image.java
@@ -5,10 +5,84 @@
  */
 package edu.spcollege.ecox.domain;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
 /**
  *
- *
+ * @author James
  */
-public class Image {
-    
+@Entity
+public class Image implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final long id;
+
+    private final String name;
+    private final Timestamp timestamp;
+    private final byte[] bytes;
+
+    public Image() {
+        this.id = 0;
+        this.name = "";
+        this.timestamp = null;
+        this.bytes = null;
+    }
+
+    public Image(long id, byte[] bytes, String name, Timestamp timeStamp) {
+        this.bytes = bytes;
+        this.name = name;
+        this.timestamp = timeStamp;
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        boolean testConclusion = false;
+
+        //Testing for null object or different class
+        if (object == null || object.getClass() != getClass()) {
+            testConclusion = false;
+        } else {
+
+            //Casting object to Image and assigning reference
+            Image image = (Image) object;
+
+            //Comparing name and timestamp to determine uniqueness
+            if (this.name == image.name && this.timestamp == image.timestamp) {
+                testConclusion = true;
+            }
+        }
+
+        return testConclusion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 7 * hash + this.name.hashCode();
+        hash = 7 * hash + this.timestamp.hashCode();
+        return hash;
+    }
+
+    public byte[] getBytes() {
+        return bytes;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
 }
--- src/main/java/edu/spcollege/ecox/domain/Location.java
+++ src/main/java/edu/spcollege/ecox/domain/Location.java
@@ -5,10 +5,71 @@
  */
 package edu.spcollege.ecox.domain;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  *
- * @author Trevor
+ * @author James
  */
-public class Location {
-    
+@Entity
+public class Location implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private final long id;
+    private final float latitude;
+    private final float longtitude;
+
+    public Location() {
+        this.id = 0;
+        this.latitude = 0;
+        this.longtitude = 0;
+    }
+
+    public Location(long id, float latitude, float longtitude) {
+        this.id = id;
+        this.latitude = latitude;
+        this.longtitude = longtitude;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Float.floatToIntBits(this.latitude);
+        hash = 37 * hash + Float.floatToIntBits(this.longtitude);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Location other = (Location) obj;
+        if (Float.floatToIntBits(this.latitude) != Float.floatToIntBits(other.latitude)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.longtitude) != Float.floatToIntBits(other.longtitude)) {
+            return false;
+        }
+        return true;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public float getLongtitude() {
+        return longtitude;
+    }
 }
