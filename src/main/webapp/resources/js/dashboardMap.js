458f9cdc824f5d91ee71a9adfdc634b31a9c6ae4
thuffsmith <huffsmith07@gmail.com>
2016-07-05 16:00:12 -0400

    Changed map tiles to a satallite style, increased vertical size of map

--- src/main/webapp/resources/js/dashboardMap.js
+++ src/main/webapp/resources/js/dashboardMap.js
@@ -3,12 +3,12 @@ This Sets the map tile - there is not a tile currently added.
  */
 
 var mymap = L.map( 'mapid', {
-    center: [20.0, 5.0],
+    center: [27.812295, -82.786255],
     minZoom: 2,
-    zoom: 2
+    zoom: 17
 });
 
-L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
+    subdomains: ['oatile1','oatile2','oatile3','oatile4']
 }).addTo( mymap );
