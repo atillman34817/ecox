42df8b25aad319eae13a5d2cafa4e945a902000c
Douglas <Douglas@gitRdoug>
2016-07-06 15:11:45 -0400

    Added demo marker to map

--- src/main/webapp/resources/js/dashboardMap.js
+++ src/main/webapp/resources/js/dashboardMap.js
@@ -8,7 +8,10 @@ var mymap = L.map( 'mapid', {
     zoom: 17
 });
 
+
 L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
     subdomains: ['oatile1','oatile2','oatile3','oatile4']
 }).addTo( mymap );
+
+L.marker([27.812295, -82.786255]).addTo(mymap).bindPopup("<b>Test Sensor</b><br>I am a test");
