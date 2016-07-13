3386cbb6b220c41608d09e73751b59546bfe2a9b
thuffsmith <huffsmith07@gmail.com>
2016-07-13 00:13:38 -0400

    Replaced Leaflet with Google Maps

--- src/main/webapp/resources/js/dashboardMap.js
+++ src/main/webapp/resources/js/dashboardMap.js
@@ -1,17 +1,12 @@
-/* 
-This Sets the map tile - there is not a tile currently added. 
- */
-
-var mymap = L.map( 'mapid', {
-    center: [27.812295, -82.786255],
-    minZoom: 2,
-    zoom: 17
-});
-
-
-L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['oatile1','oatile2','oatile3','oatile4']
-}).addTo( mymap );
-
-L.marker([27.812295, -82.786255]).addTo(mymap).bindPopup("<b>Test Sensor</b><br>I am a test");
+function initMap() {
+    var location = new google.maps.LatLng(27.812124, -82.784386);
+    
+    var mapSettings = {
+        zoom: 16,
+        center: location,
+        mapTypeId: google.maps.MapTypeId.HYBRID
+    };
+    
+    var sensorMap = new google.maps.Map(document.getElementById('sensorMap'),
+        mapSettings);
+}
